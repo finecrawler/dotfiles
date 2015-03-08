8a4f7b5926916b750e2a7c839da33f278246cbd3
Louis Bloom <finecrawler@gmail.com>
2015-03-08 16:32:46 +0800

    disable toolbar and change frame size for emacs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,3 +23,22 @@
 (load "07yasnippet.el")
 (load "08solarized.el")
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default-frame-alist
+    (quote
+     ((cursor-color . "red")
+      (vertical-scroll-bars)
+      (height . 50)
+      (width . 90))))
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(mode-line ((t (:foreground "#030303" :background "#bdbdbd" :box nil))))
+ '(mode-line-inactive ((t (:foreground "#f9f9f9" :background "#666666" :box nil)))))
