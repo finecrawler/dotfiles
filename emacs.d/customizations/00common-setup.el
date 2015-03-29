6b23871017fb04f77a4a4461a20afe38ac783e55
Louis Bloom <finecrawler@gmail.com>
2015-03-29 15:02:43 +0800

    some tweaks

--- emacs.d/customizations/00common-setup.el
+++ emacs.d/customizations/00common-setup.el
@@ -148,3 +148,7 @@
   (toggle-read-only))
 
 (add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
